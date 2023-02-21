cf6c132c130478df818eb65df12626747a83bd0a
Simi <simimortay@gmail.com>
2023-02-21 09:00:54 -0800

    Add Integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * add - add two integers
- * @x: int type number
- * @y: int type number
+ * add two integers
+ * @a: int type number
+ * @b: int type number
  * Return: result of addition
  */
+
 int add(int a, int b)
 {
 	return (a + b);
