fdee14c58a7464bb71abd3ce7fb8ba7af0b999cf
Simi <simimortay@gmail.com>
2023-02-23 13:19:38 -0800

    More Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * function that prints 10 times the numbers, 
+ * from 0 to 14, followed by a new line.
+ */
+
+
+void more_numbers(void)
+{
+	int i;
+	int h;
+
+	for (h = 0; h <= 9; h++)
+	{
+	for (i = 0; i <= 14; i++)
+	{
+	if (i > 9)
+	{
+		_putchar(i / 10 + '0');
+	}
+	_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+	}
+
+}
