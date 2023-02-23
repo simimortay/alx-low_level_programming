c066c8f60a90e4d0860c18c09efddc02af6defd6
Simi <simimortay@gmail.com>
2023-02-23 12:53:03 -0800

    Integer Multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * function that multiplies two integers.
+ * a= first number
+ * b= second number
+ * Return 0
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
